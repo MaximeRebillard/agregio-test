8a696200b6ac6dce885a254bb6ae79e63de6bd7f
Maxime REBILLARD <maxime.rebillard@glady.com>
2023-03-18 14:58:23 +0100

    feat(tech) : add psvm

---
+++ agregio.technical-interview/src/main/java/com/rebillard/Main.java
@@ -0,0 +1,15 @@
+package com.rebillard;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.annotations.QuarkusMain;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@QuarkusMain
+public class Main {
+
+  public static void main(String... args) {
+    log.info("Running main method");
+    Quarkus.run(args);
+  }
+}
